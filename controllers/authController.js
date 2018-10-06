82919e629ca5204e2fd7f5f65b93933f748bdbac
colmlg <colm.legear@gmail.com>
2018-10-06 17:48:18 +0100

    Changed status codes

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,12 +45,12 @@ exports.verifyToken = function(req, res, next) {
     var token = req.headers['x-access-token'];
 
     if (!token) {
-        return res.status(403).send({ error: 'No token provided.'});
+        return res.status(401).send({ error: 'No token provided.'});
     }
 
     jwt.verify(token, process.env.TOKEN_SECRET, function(err, decoded) {
         if (err) {
-            return res.status(500).send({ error: 'Invalid token.'});
+            return res.status(401).send({ error: 'Invalid token.'});
         }
 
         req.userId = decoded.id;
