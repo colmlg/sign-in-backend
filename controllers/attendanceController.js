ba137a9cd2cc792e9d29b6223295780daba51baf
colmlg <colm.legear@gmail.com>
2018-10-06 17:41:47 +0100

    Compare date and time for module event

---
+++ controllers/attendanceController.js
@@ -0,0 +1,50 @@
+var Module = require('../models/module');
+var Event = require('../models/event');
+var moment = require('moment');
+require('moment-recur');
+
+exports.markAttendance = function(req, res) {
+    Module.findOne({ id: req.body.moduleId }).populate('events').exec(function (error, module) {
+        if (error) {
+            return res.status(500).json(error);
+        }
+
+        if(!module) {
+            return res.status(404).send();
+        }
+
+        if(module.students.indexOf(req.userId) === -1) {
+            return res.status(403).json({ error: 'You are not registered in this module.'});
+        }
+
+        for(var i = 0; i < module.events.length; i++) {
+            var event = module.events[i]._doc;
+
+            if (isEventCurrentlyOn(event) && event.roomNumber === req.body.roomNumber) {
+                return res.status(200).json({occurring: true, event: event});
+            }
+        }
+
+        return res.status(200).json({ occurring: false });
+
+    });
+};
+
+var isEventCurrentlyOn = function(event) {
+    var recurrence = moment().recur({
+        start: event.startDate,
+        end: event.startDate
+    }).every(1).weeks();
+
+    var today = moment().format("MM/DD/YYYY");
+
+    //Moment-recur doesn't store any time information, so we have to check that separately
+    var eventTime = moment(event.startTime, 'hh:mm').hours();
+    var currentTime= moment().hours();
+    console.log(eventTime);
+
+    var dateMatches = recurrence.matches(today);
+    var timeMatches = currentTime >= eventTime && currentTime <= (eventTime + event.duration)
+
+    return dateMatches && timeMatches;
+};
